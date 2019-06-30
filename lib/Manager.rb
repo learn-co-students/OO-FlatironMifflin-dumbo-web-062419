02d584a94478de045b4218de8c010a967f116d12
lgm527 <laurellmccaffrey@gmail.com>
2019-06-30 13:54:39 -0400

    Micheal will be pleased, also jim sucks

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,44 @@
 class Manager
 
+  attr_accessor :name, :department, :age, :employees
+  @@all = []
+
+  def initialize(name, department)
+    @name = name
+    @department = department
+    @age = age
+    @employees = []
+    @@all << self
+  end
+
+  def hire_employee(name, salary)
+    new_guy = Employee.new(name, salary, self.department)
+    @employees << new_guy
+    new_guy
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.all_departments
+    self.all.map do |manager|
+      manager.department
+    end
+  end
+
+  def self.average_age
+    age_sum = 0
+    @@all.each do |manager|
+      age_sum += manager.age
+    end
+    age_sum/@@all.length
+  end
+
+  def employees
+    Employee.all.select do |employee|
+      employee.department == self.department
+    end
+  end
+
 end
--- run.rb
+++ run.rb
@@ -2,8 +2,23 @@ require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
 
+#Employee.new(name, salary, department)
+#Manager.new(name, department)
+oscar = Manager.new("Oscar", "Finance")
+oscar.age = 34
+jim = Manager.new("Jim", "Sales")
+jim.age = 30
+toby = Manager.new("Toby", "Human Resources")
+toby.age = 39
 
-#Test your code here
+
+angela = oscar.hire_employee("Angela", 60000)
+kevin = oscar.hire_employee("Kevin", 55000)
+pam = jim.hire_employee("Pam", 54500)
+kelly = toby.hire_employee("Kelly", 45000)
+gabe = toby.hire_employee("Gabe", 70000)
+
+
 
 
 binding.pry
