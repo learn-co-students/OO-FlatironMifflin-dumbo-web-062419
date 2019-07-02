38f25771cb450dde33aad85ab1507fe9ae799853
Peter Lin <peterlin5301997@gmail.com>
2019-07-02 09:19:13 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,39 @@
 class Employee
+    attr_accessor :manager
+    attr_reader :name, :salary
+
+    @@all = []
+
+    def initialize(name, salary)
+        @name = name
+        @salary = salary
+        @@all << self
+    end
+
+    def manager_name
+        @manager.name
+    end
+
+    def tax_bracket
+        @@all.select do |employee|
+            employee.salary < self.salary + 1000 || employee.salary > self.salary - 1000
+        end
+    end
+
+    def Employee.all
+        @@all
+    end
+
+    def Employee.paid_over(amount)
+        @@all.select do |employee|
+            employee.salary > amount
+        end
+    end
+
+    def Employee.find_by_department(dept_name)
+        @@all.find do |employee|
+            employee.manager.department == dept_name
+        end
+    end
 
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,14 @@ require 'pry'
 
 #Test your code here
 
+manager1 = Manager.new("Bob", "Marketing", 28)
+manager2 = Manager.new("Abby", "Finance", 24)
+employee1 = Employee.new("John", 1600)
+employee2 = Employee.new("Will", 1890)
+manager1.hire_employee("John", 1600)
+manager1.hire_employee("Will", 1890)
+
+
 
 binding.pry
 puts "done"
